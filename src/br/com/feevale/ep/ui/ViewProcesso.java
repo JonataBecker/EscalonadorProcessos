e3691c19a12c19da324e49ac65f9069f7c965442
JonataBecker <jonatabecker@gmail.com>
2016-09-27 18:42:15 -0300

    Criado view de processo

---
+++ src/br/com/feevale/ep/ui/ViewProcesso.java
@@ -0,0 +1,15 @@
+package br.com.feevale.ep.ui;
+
+import javafx.scene.layout.BorderPane;
+
+/**
+ *
+ */
+public class ViewProcesso extends BorderPane {
+
+    public ViewProcesso() {
+        setStyle("-fx-background-color:blue");
+        setPrefSize(150, 100);
+    }
+    
+}
